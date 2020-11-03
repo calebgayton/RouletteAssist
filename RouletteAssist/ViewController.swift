6b7102b72f5df9de3439c1e94f7e9a15c639c38c
Caleb Gayton <>
2020-11-03 14:20:53 -0500

    first commit

---
+++ RouletteAssist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RouletteAssist
+//
+//  Created by Caleb Gayton on 11/3/20.
+//  Copyright © 2020 Caleb Gayton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
